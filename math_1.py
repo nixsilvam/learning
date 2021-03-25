659b3f15edef453ee2c04765ed205797eb0edda4
nixsilvam <81374946+nixsilvam@users.noreply.github.com>
2021-03-25 17:55:43 +0200

    Add files via upload

---
+++ math_1.py
@@ -0,0 +1,9 @@
+import math
+x = input('x:')
+y = input('y:')
+print('x+y=:', int(x)+int(y))
+print('x//y=:', int(x)//int(y))
+print('remainder:', int(x) % int(y))
+print('x*y=', int(x)*int(y))
+
+
---
+++ myname.py
@@ -0,0 +1,25 @@
+from art import *
+tprint('WOW', font="random")
+
+art_2 = art("woman", number=3)
+print(art_2)
+
+print(art("random"))
+
+aprint("woman", number=13)
+
+tprint('IANA', font="alligator")
+
+tprint("magic", font="fancy94", decoration="rand")
+
+print('    *        *         *       *        *')
+print('    *       * *        **      *       * * ')
+print('    *      *   *       * *     *      *   *')
+print('    *     *     *      *  *    *     *     *')
+print('    *    *       *     *   *   *    *       *')
+print('    *   ************        *  *   ***********')
+print('    *  *           *   *     * *  *           *')
+print('    * *             *  *      ** *             *')
+print('    * *              * *       * *              *')
+
+
