aeb44406c35bc128ef1865e5e412625ef7b4bb3f
nixsilvam <81374946+nixsilvam@users.noreply.github.com>
2021-05-06 12:34:21 +0300

    Update les_11_task_2.py

--- les_11_task_2.py
+++ les_11_task_2.py
@@ -1,4 +1,5 @@
-def hide_email(mail: str):
+def hide_email(mail: str) -> str:
+    """Hide e-mail lol"""
     mail_name = mail.split('@')[0]
     hosting_name = mail.split('@')[1]
     return mail_name[:2] + '*' * (len(mail_name) - 2) + '@' + '*' * (len(hosting_name) - 4) + hosting_name[-4:]
