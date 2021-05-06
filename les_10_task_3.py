8a831179022a3f05f1ec3dc2d0e73a5cfc7b18e5
nixsilvam <81374946+nixsilvam@users.noreply.github.com>
2021-05-06 12:24:16 +0300

    Update les_10_task_3.py

--- les_10_task_3.py
+++ les_10_task_3.py
@@ -6,10 +6,11 @@ def shift(spsk, stp):
     if stp < 0:
         stp = abs(stp)
         for n in range(stp):
-            copy_list.append(spsk.pop(0))
+            copy_list.append(copy_list.pop(0))
     else:
         for n in range(stp):
-            copy_list.insert(0, spsk.pop())
+            copy_list.insert(0, copy_list.pop())
+    return copy_list
 
 
 lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
